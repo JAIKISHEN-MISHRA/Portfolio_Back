d42cd7a6c992072442648421136cae8d1f1dca4b
JAIKISHEN-MISHRA <119570134+JAIKISHEN-MISHRA@users.noreply.github.com>
2023-08-27 10:48:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient 
 import json
 
 app = Flask(__name__)
 CORS(app)
 
-
-client = MongoClient('mongodb+srv://Jaimishra20031:Jai31072003@jaicluster.xau2qru.mongodb.net/?retryWrites=true&w=majority')  
-db = client['form_data'] 
-collection = db['submissions']  
-
 @app.route('/api/submit-form', methods=['POST'])
 def submit_form():
     if request.method == 'POST':
-        form_data = request.json
+        try:
+            form_data = request.json
 
-        collection.insert_one(form_data)
+            if form_data is None:
+                raise ValueError("No JSON data found in the request body.")
 
-        response_data = {"message": "Form data received and saved successfully"}
-        return jsonify(response_data)
+            # Save form data to a JSON file
+            with open('form_data.json', 'a') as json_file:
+                json.dump(form_data, json_file)
+                json_file.write('\n')
+
+            response_data = {"message": "Form data received and saved to JSON file successfully"}
+        except Exception as e:
+            response_data = {"error": str(e)}
+
+        response = jsonify(response_data)
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')  # Allow requests from your frontend
+        response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')  # Allow specific methods
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')  # Allow specific headers
+
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
